3529451f41ce6f8112da4deb35b7fd6b922fac04
Daria Zolotoreva <dasha_vid@mail.ru>
2024-01-23 13:49:11 +0200

    Initial commit

---
+++ jest/jest.config.js
@@ -0,0 +1,13 @@
+const config = {
+  collectCoverageFrom: [
+    "**/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+  ],
+
+  coverageThreshold: {
+    branches: 100,
+    functions: 100,
+    lines: 100,
+  },
+};
---
+++ jest/test/unit/sortByName.test.js
@@ -0,0 +1,24 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+   const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ]
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]
+    const output =  sorting.sortByName(input)
+   
+    expect(output).toEqual(expected);
+  });
+
+  test("Without params throws exception", () => {
+    expect(() => sorting.sortByName()).toThrow(TypeError)
+  })
+});
+
