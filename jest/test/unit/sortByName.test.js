04d180b40c3cc08187841e4abaf36e34cc40545e
Daria Zolotoreva <dasha_vid@mail.ru>
2024-01-24 14:22:45 +0200

    First corrections added

--- jest/test/unit/sortByName.test.js
+++ jest/test/unit/sortByName.test.js
@@ -2,23 +2,34 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-   const input = [
+    const input = [
       "Гарри Поттер",
       "Властелин Колец",
       "Волшебник изумрудного города",
-    ]
+    ];
     const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]
-    const output =  sorting.sortByName(input)
-   
+    ];
+    const output = sorting.sortByName(input);
+
     expect(output).toEqual(expected);
   });
 
   test("Without params throws exception", () => {
-    expect(() => sorting.sortByName()).toThrow(TypeError)
-  })
+    expect(() => sorting.sortByName()).toThrow(TypeError);
+  });
 });
 
+describe("Books names test suit negative", () => {
+  it("Naming a workbook without sorting", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Волшебник изумрудного города",
+        "Гарри Поттер",
+      ])
+    ).toEqual(["Волшебник изумрудного города", "Гарри Поттер", "Гарри Поттер"]);
+  });
+});
--- playwright/tests/tests.spec.js
+++ playwright/tests/tests.spec.js
@@ -4,15 +4,14 @@ const user = require("../user");
 test("test", async ({ page }) => {
   await page.goto("https://netology.ru/?modal=sign_in");
   await page.locator('[placeholder="Email"]').click();
-  await page.locator('[placeholder="Email"]').fill(user.email);
+  await page.locator('[placeholder="Email"]').fill(user.userEmail);
   await page.locator('[placeholder="Пароль"]').click();
-  await page.locator('[placeholder="Пароль"]').fill(user.password);
+  await page.locator('[placeholder="Пароль"]').fill(user.userPassword);
   await page.locator('[data-testid="login-submit-btn"]').click();
-  await page.waitForURL("https://netology.ru/profile");
+  await page.waitForURL("https://netology.ru/profile/8672827");
   await page.screenshot({ path: "screenshot.png" });
   const header = await page.locator("h2").first();
-  await expect(header).toHaveText("Мои курсы и профессии");
-  await browser.close();
+  await expect(header).toHaveText("Моё обучение");
 });
 
 test("not a valid test", async ({ page }) => {
@@ -26,5 +25,4 @@ test("not a valid test", async ({ page }) => {
     "Вы ввели неправильно логин или пароль"
   );
   await page.screenshot({ path: "screenshotError.png" });
-  await browser.close();
 });
